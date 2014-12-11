eb0a54e21bb2421faee55fe642297e67a9a01261
Andreas Bieber <andreas.bieber@1und1.de>
2014-12-11 18:37:46 +0100

    removed duplicated assemblyconfiguration attribute

--- SignalR.Client.TypedHubProxy/Properties/AssemblyInfo.cs
+++ SignalR.Client.TypedHubProxy/Properties/AssemblyInfo.cs
@@ -17,7 +17,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
