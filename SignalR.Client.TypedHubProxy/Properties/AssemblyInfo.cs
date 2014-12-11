9e2b3d8ae7a2a7c71257df0b8170d5eaee54d8ac
Andreas Bieber <andreas.bieber@1und1.de>
2014-12-11 13:46:56 +0100

    prepared for appveyor

--- SignalR.Client.TypedHubProxy/Properties/AssemblyInfo.cs
+++ SignalR.Client.TypedHubProxy/Properties/AssemblyInfo.cs
@@ -12,6 +12,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (c) 1&1 Internet AG")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,14 +27,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f45fcb60-7e77-4439-8eac-b254f57a3978")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0")]
+// The following version numbers will be set/increased by the appveyor build process
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
