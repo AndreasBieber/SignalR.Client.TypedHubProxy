07d85a620e02222fd4ffb453375254493aa87c69
Pavel Platto <hinidu@gmail.com>
2014-12-15 18:04:47 +0200

    Add basic test for ObservableHubProxy

---
+++ SignalR.Client.TypedHubProxy.Tests/ObservableHubProxyTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace SignalR.Client.TypedHubProxy.Tests
+{
+    public class ObservableHubProxyTests : IUseFixture<ObservableHubProxyFixture>
+    {
+        private ObservableHubProxyFixture _testFixture;
+
+        public void SetFixture(ObservableHubProxyFixture data)
+        {
+            _testFixture = data;
+        }
+
+        [Fact]
+        public async Task ObserveEvent()
+        {
+            Guid id = Guid.NewGuid();
+
+            var responseId = _testFixture.HubProxy.Observe<Guid>(hub => hub.DelayedAnswerFromServer).FirstAsync();
+            _testFixture.HubProxy.Call(hub => hub.SendDelayedWithMs(id, 10));
+            (await responseId).Should().Be(id);
+        }
+    }
+}
